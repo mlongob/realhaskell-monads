53fd0e5572cfc83e6d8f364a6594b42767cbd1aa
Mario Longobardi <mlongobardi2@bloomberg.net>
2016-01-13 20:08:49 -0500

    Implement IO Monad wrapper for safer handle containment

--- HandleIO.hs
+++ HandleIO.hs
@@ -12,10 +12,28 @@ module HandleIO (
 
 import System.IO (Handle, IOMode(..))
 import qualified System.IO
+import Control.Monad.Trans (MonadIO(..))
+import System.Directory (removeFile)
 
 newtype HandleIO a = HandleIO { runHandleIO :: IO a }
-    deriving (Functor, Applicative, Monad)
+    deriving (Functor, Applicative, Monad, MonadIO)
 
-openFile = undefined
-hClose = undefined
-hPutStrLn = undefined
+openFile :: FilePath -> IOMode -> HandleIO Handle
+openFile path mode = HandleIO $ System.IO.openFile path mode
+
+hClose :: Handle -> HandleIO ()
+hClose = HandleIO . System.IO.hClose
+
+hPutStrLn :: Handle -> String -> HandleIO ()
+hPutStrLn h s = HandleIO $ System.IO.hPutStrLn h s
+
+safeHello :: FilePath -> HandleIO ()
+safeHello path = do
+    h <- openFile path WriteMode
+    hPutStrLn h "hello world"
+    hClose h
+
+tidyHello :: FilePath -> HandleIO ()
+tidyHello path = do
+    safeHello path
+    liftIO (removeFile path)
---
+++ MonadHandleIO.hs
@@ -0,0 +1,17 @@
+{-# LANGUAGE MultiParamTypeClasses #-}
+
+module MonadHandleIO where
+
+import MonadHandle
+import qualified System.IO
+import System.IO (IOMode(..))
+import Control.Monad.Trans (MonadIO(..), MonadTrans(..))
+import System.Directory (removeFile)
+
+
+instance MonadHandle System.IO.Handle IO where
+    openFile = System.IO.openFile
+    hPutStr = System.IO.hPutStr
+    hClose = System.IO.hClose
+    hGetContents = System.IO.hGetContents
+    hPutStrLn = System.IO.hPutStrLn
